b222a77f1fe4aa00d8ca6883323e2671c87fbd9f
BlueTot <bluetotkc@gmail.com>
2025-07-22 18:51:16 +0100

    feat: added sample template config directory

---
+++ static/katex-macros.js
@@ -0,0 +1,9 @@
+window.katexMacros = {
+    "\\R": "\\mathbb{R}",
+    "\\C": "\\mathbb{C}",
+    "\\Q":"\\mathbb{Q}",
+    "\\Z": "\\mathbb{Z}",
+    "\\N": "\\mathbb{N}",
+    "\\v": "\\vec",
+    "\\b": "\\textbf",
+};
